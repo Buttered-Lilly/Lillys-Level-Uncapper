ea1c8378386dd2c368204822e9eaac52c44113c7
ButteredLilly <BrandonBarrisoff@outlook.com>
2024-11-29 00:27:08 -0800

    Add project files.

---
+++ Core.cs
@@ -0,0 +1,27 @@
+﻿using MelonLoader;
+using HarmonyLib;
+
+[assembly: MelonInfo(typeof(Lillys_Level_Uncapper.Core), "Lillys Level Uncapper", "1.0.0", "Lilly", null)]
+[assembly: MelonGame("KisSoft", "ATLYSS")]
+
+namespace Lillys_Level_Uncapper
+{
+    public class Core : MelonMod
+    {
+        [HarmonyPatch(typeof(GameManager), "Start")]
+        public static class gamemanager
+        {
+            private static void Postfix(ref GameManager __instance)
+            {
+                try
+                {
+                    __instance._statLogics._maxMainLevel = 10000;
+                }
+                catch (Exception e)
+                {
+                    MelonLogger.Msg(e);
+                }
+            }
+        }
+    }
+}
